44a8172025f0602c2fbce2662380df8b2d3b5182
pappukrs <digitalpappu101@gmail.com>
2026-03-06 22:57:05 +0530

    recursive paindrome check

---
+++ recursion/palindrome.js
@@ -0,0 +1,14 @@
+function isPalindrome(str) {
+    if (str.length <= 1) {
+        return true;
+    }
+
+    if (str[0] !== str[str.length - 1]) {
+        return false;
+    }
+
+    return isPalindrome(str.slice(1, -1));
+}
+
+console.log(isPalindrome("madam")); // true
+console.log(isPalindrome("hello")); // false
