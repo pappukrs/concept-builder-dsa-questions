7badc7c010ba55159c701b767f34f0a67a1fd3b1
pappukrs <digitalpappu101@gmail.com>
2026-03-07 22:35:00 +0530

    max depth

---
+++ recursion/maxDepth.js
@@ -0,0 +1,10 @@
+function maxDepth(root) {
+    if (root === null) {
+        return 0;
+    }
+
+    let left = maxDepth(root.left);
+    let right = maxDepth(root.right);
+
+    return Math.max(left, right) + 1;
+}
