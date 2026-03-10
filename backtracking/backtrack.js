6881aad988441aca7d7cf2de36ca99040b56cc62
pappukrs <digitalpappu101@gmail.com>
2026-03-10 08:48:46 +0530

    backtrack mental map

---
+++ backtracking/backtrack.js
@@ -0,0 +1,16 @@
+function backtrack(state) {
+
+    if (solution found) {
+    save solution
+        return
+    }
+
+    for (choice of choices) {
+
+    make choice
+
+        backtrack(next state)
+
+    undo choice
+    }
+}
