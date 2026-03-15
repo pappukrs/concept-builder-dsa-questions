1d4efa9cf0cacda725b8246c2becb2a28dc4ef8b
pappukrs <digitalpappu101@gmail.com>
2026-03-15 23:12:30 +0530

    max subarray

---
+++ dp/maxSubArray.js
@@ -0,0 +1,17 @@
+function maxSubArray(nums) {
+
+    let current = nums[0]
+    let maxSum = nums[0]
+
+    for (let i = 1; i < nums.length; i++) {
+
+        current = Math.max(nums[i], current + nums[i])
+
+        maxSum = Math.max(maxSum, current)
+
+    }
+
+    return maxSum
+}
+
+console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
