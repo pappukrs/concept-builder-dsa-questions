aeb6c10219330e4c6896ca4d669ba8796593c10f
pappukrs <digitalpappu101@gmail.com>
2026-03-16 03:02:03 +0530

    bst

---
+++ bst/searchBST.js
@@ -0,0 +1,9 @@
+function searchBST(root, val) {
+    if (!root || root.val === val) return root
+
+    if (val < root.val) {
+        return searchBST(root.left, val)
+    } else {
+        return searchBST(root.right, val)
+    }
+}
