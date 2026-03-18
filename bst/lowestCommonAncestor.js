7d0006b93c2e6a12d421d2a4893da72444eaa046
pappukrs <digitalpappu101@gmail.com>
2026-03-18 22:06:06 +0530

    lowest common ancesstors

---
+++ bst/lowestCommonAncestor.js
@@ -0,0 +1,19 @@
+function lowestCommonAncestor(root, p, q) {
+
+    while (root) {
+
+        if (p.val < root.val && q.val < root.val) {
+            root = root.left
+        }
+
+        else if (p.val > root.val && q.val > root.val) {
+            root = root.right
+        }
+
+        else {
+            return root
+        }
+
+    }
+
+}
