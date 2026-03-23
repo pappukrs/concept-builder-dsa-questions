9241c809d8f4616d679632e595ce6ed0648b6779
pappukrs <digitalpappu101@gmail.com>
2026-03-23 21:56:40 +0530

    hasPathSum

---
+++ bst/hasPathSum.js
@@ -0,0 +1,11 @@
+function hasPathSum(root, targetSum) {
+
+    if (!root) return false
+
+    if (!root.left && !root.right) {
+        return targetSum === root.val
+    }
+
+    return hasPathSum(root.left, targetSum - root.val) ||
+        hasPathSum(root.right, targetSum - root.val)
+}
